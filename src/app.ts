69848710effda2584e90939d50f1ebb7e4dc7cba
kmebin <blowhuiee@gmail.com>
2022-01-30 19:47:18 +0900

    [ADD] Object, Array, Tuple 타입

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log('');
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: '김희빈',
+  age: 25,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
+}
+
+let activities: string[];
+
+activities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
