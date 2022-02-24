d6b83d23c0d8e76f47d05d45969ade32824b8023
kmebin <blowhuiee@gmail.com>
2022-02-24 15:13:13 +0900

    [ADD] 리터럴 타입

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-string'
+) {
   let result;
 
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
@@ -10,7 +14,8 @@ function combine(input1: number | string, input2: number | string) {
   return result;
 }
 
-const combinedAge = combine(30, 26);
-const combinedName = combine('Max', 'Anna');
+const combinedAge = combine(30, 26, 'as-number');
+const combinedName = combine('Max', 'Anna', 'as-string');
+const combinedStringAge = combine('30', '26', 'as-number');
 
-console.log(combinedAge, combinedName);
+console.log(combinedAge, combinedName, combinedStringAge);
