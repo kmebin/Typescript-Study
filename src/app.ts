9f6f1e8a17becbce36a07df209ec7a47cbc0b817
kmebin <blowhuiee@gmail.com>
2022-02-24 15:01:12 +0900

    [ADD] Union 타입

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
-enum Role { ADMIN, READ_ONLY, AUTHOR };
+function combine(input1: number | string, input2: number | string) {
+  let result;
 
-const person = {
-  name: '김희빈',
-  age: 25,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.AUTHOR
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+  return result;
 }
 
-let activities: string[];
+const combinedAge = combine(30, 26);
+const combinedName = combine('Max', 'Anna');
 
-activities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
+console.log(combinedAge, combinedName);
