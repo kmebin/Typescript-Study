f3fe98062fdb37a11d13e3c56b8c31133690804b
kmebin <blowhuiee@gmail.com>
2022-04-06 12:37:55 +0900

    [CHORE] 폴더링 수정

---
+++ src/section4/modernJs.ts
@@ -0,0 +1,56 @@
+/**
+ * @화살표_함수
+ */
+const add = (a: number, b = 1) => a + b;
+const printOutput = (output: number) => console.log(output);
+const printOutput2: (a: number) => void = output => console.log(output); // 함수의 타입을 명시해주는 방법
+
+printOutput(add(5));
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', () => {}); // 익명 함수
+  button.addEventListener('click', event => console.log(event));
+}
+
+/**
+ * @전개_연산자
+ */
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+const activeHobbies2 = ['Hiking', ...hobbies]
+
+activeHobbies.push(...hobbies); // push로도 가능
+
+const person = {
+  name: 'Max',
+  age: 30
+};
+const notCopiedPerson = person;
+const copiedPerson = { ...person };
+
+/**
+ * @나머지_매개변수
+ */
+const addNumber = (...numbers: number[]) => {
+  const result = numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+
+  return result;
+};
+const addedNumbers = addNumber(5, 10, 2, 3.7);
+
+console.log(addedNumbers);
+
+/**
+ * @비구조화_할당
+ */
+const [hobby1, hobby2, ...remainingHobbies] = activeHobbies;
+
+console.log(activeHobbies, hobby1, hobby2, remainingHobbies);
+
+const { name: firstName, age } = person;
+
+console.log(firstName, age);
