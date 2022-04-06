410494d7d1de83a18c91193bb9f33ec4794a9495
kmebin <blowhuiee@gmail.com>
2022-04-06 14:11:28 +0900

    [ADD] private 접근 지정자 추가

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@
  * @클래스
  */
 class Department {
-  name: string;
+  private name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -11,12 +12,21 @@ class Department {
   describe(this: Department) {
     console.log(`Department: ${this.name}`)
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
 accounting.describe();
-
-const accountingCopy = { name: 'Dummy', describe: accounting.describe };
-
-accountingCopy.describe();
+accounting.printEmployeeInformation();
