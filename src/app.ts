4cbf6afce38fd66a25245b64ba27d35bf44d14cc
kmebin <blowhuiee@gmail.com>
2022-04-07 13:43:06 +0900

    [ADD] 인터페이스

--- src/app.ts
+++ src/app.ts
@@ -1,109 +1,16 @@
-abstract class Department {
-  static fiscalYear = 2022;
-  protected employees: string[] = [];
+interface Person {
+  name: string;
+  age: number;
 
-  constructor(protected readonly id: string, private name: string) {}
-
-  static createEmployee(name: string) {
-    return { name };
-  }
-
-  abstract describe(this: Department): void;
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+  greet(phrase: string): void;
 }
 
-const employee1 = Department.createEmployee('Max');
+const user1: Person = {
+  name: 'Max',
+  age: 30,
+  greet(phrase: string) {
+    console.log(`${phrase} ${this.name}`);
+  },
+};
 
-console.log(employee1);
-console.log(Department.fiscalYear);
-
-class ITDepartment extends Department {
-  constructor(id: string, private admins: string[]) {
-    super(id, 'IT');
-  }
-
-  describe() {
-    console.log(`IT Department - ID: ${this.id}`);
-  }
-
-  printAdmins() {
-    console.log(this.admins);
-  }
-}
-
-class AccountingDepartment extends Department {
-  private static instance: AccountingDepartment;
-  private lastReport: string;
-
-  get mostRecentReport() {
-    if (this.lastReport) return this.lastReport;
-    throw new Error('No report found.');
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) throw new Error('Please pass in a valid value.');
-    this.addReport(value);
-  }
-
-  private constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting');
-    this.lastReport = reports[0];
-  }
-
-  static getInstance() {
-    if (this.instance) return this.instance;
-    this.instance = new AccountingDepartment('d2', []);
-
-    return this.instance;
-  }
-
-  describe() {
-    console.log(`Accounting Department - ID: ${this.id}`);
-  }
-
-  addEmployee(employee: string) {
-    if (employee === 'Max') return;
-    this.employees.push(employee);
-  }
-
-  addReport(report: string) {
-    this.reports.push(report);
-    this.lastReport = report;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-}
-
-const it = new ITDepartment('d1', ['Max']);
-
-it.addEmployee('Max');
-it.addEmployee('Manu');
-
-it.describe();
-it.printEmployeeInformation();
-it.printAdmins();
-
-const accounting = AccountingDepartment.getInstance();
-const accounting2 = AccountingDepartment.getInstance();
-
-console.log(accounting, accounting2);
-
-accounting.mostRecentReport = 'Year End Report';
-console.log(accounting.mostRecentReport);
-
-accounting.addReport('Something went wrong...');
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-
-accounting.printReports();
-accounting.printEmployeeInformation();
+user1.greet('Hi there - I am');
