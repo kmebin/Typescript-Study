9d5c75874274189395c6cec78847339d75270fa9
kmebin <blowhuiee@gmail.com>
2022-04-07 16:02:26 +0900

    [ADD] 클래스에 인터페이스 사용

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
-interface Person {
-  name: string;
-  age: number;
+interface Greetable {
+  readonly name: string;
 
   greet(phrase: string): void;
 }
 
-const user1: Person = {
-  name: 'Max',
-  age: 30,
+class Person implements Greetable {
+  name: string;
+
+  constructor(n: string, private age: number) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(`${phrase} ${this.name}`);
-  },
-};
+  }
+}
+
+const user1 = new Person('Max', 30);
 
 user1.greet('Hi there - I am');
+console.log(user1);
