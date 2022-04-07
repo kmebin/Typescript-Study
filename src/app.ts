c88a1a7787e572cbbbfc6e09b5a31a290fffc279
kmebin <blowhuiee@gmail.com>
2022-04-07 16:34:59 +0900

    [ADD] 옵셔널 속성 추가

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,8 @@ const add2: addFuncByInterface = (n1: number, n2: number) => {
 }
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -27,18 +28,25 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
+  age = 30;
 
-  constructor(n: string, private age: number) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) this.name = n;
   }
 
   greet(phrase: string) {
-    console.log(`${phrase} ${this.name}`);
+    if (this.name) console.log(`${phrase} ${this.name}`);
+    else console.log('Hi!')
   }
 }
 
-const user1 = new Person('Max', 30);
+const user1 = new Person('Max');
 
 user1.greet('Hi there - I am');
 console.log(user1);
+
+const user2 = new Person();
+
+user2.greet('Hi there - I am');
+console.log(user2);
