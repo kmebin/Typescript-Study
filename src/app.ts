40e7f2f2b9bbdb9e299cc113d201aa215b79e46d
kmebin <blowhuiee@gmail.com>
2022-04-07 16:20:47 +0900

    [ADD] 인터페이스로 함수 정의

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
+/**
+ * @사용자_정의_타입_사용
+ */
+type addFuncByType = (a: number, b: number) => number;
+
+const add1: addFuncByType = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+/**
+ * @인터페이스_사용
+ */
+interface addFuncByInterface {
+  (a: number, b: number): number;
+}
+
+const add2: addFuncByInterface = (n1: number, n2: number) => {
+  return n1 + n2;
+}
+
 interface Named {
   readonly name: string;
 }
